01fcc211ff7b36f4c1a9e6ff5911e29b55063172
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-02-04 00:07:45 +0700

    login and logout

--- my_users/urls.py
+++ my_users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'my_users'
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='my_users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='my_users/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
--- my_users/views.py
+++ my_users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import MyUserRegisterForm
 
 
@@ -9,15 +10,20 @@ def register(request):
         form = MyUserRegisterForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}')
+            messages.success(request, f'Your Account Created Successfully!, You are now able to login')
             form.save()
 
-            return redirect('my_blog:home')
+            return redirect('my_users:login')
     else:
         form = MyUserRegisterForm()
 
     context = {
         'form': form
     }
+
     return render(request, 'my_users/register.html', context)
 
+
+@login_required
+def profile(request):
+    return render(request, 'my_users/profile.html')

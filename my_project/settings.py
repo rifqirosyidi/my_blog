88896dcb5a13d25edcd5abbbb55013ac694aac83
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-02-03 23:28:07 +0700

    crispy forms

--- my_project/settings.py
+++ my_project/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
 
     'my_blog',
     'my_users',
+
+    # Third Party
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- my_users/forms.py
+++ my_users/forms.py
@@ -1,9 +1,21 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 class MyUserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        label='First Name',
+        required=True
+    )
+
+    last_name = forms.CharField(
+        label='Last Name',
+        required=True
+    )
+
     email = forms.EmailField()
 
     class Meta:
